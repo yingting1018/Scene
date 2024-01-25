233d9b012209b9e7da49445dbd15e4043eabfa0c
Yingting Huang <yhuan300@ucsc.edu>
2024-01-24 21:52:08 -0800

    scenes

---
+++ CP-Making-a-Scene-Template-master/src/scenes/StatsOverlay.js
@@ -0,0 +1,18 @@
+class StatsOverlay extends Phaser.Scene
+{
+    constructor()
+    {
+        super('statsoverlayScene')
+        console.log('StatsOverlay: constructor')
+    }
+
+    init()
+    {
+        console.log('StatsOverlay: init')
+    }
+
+    update()
+    {
+      //  console.log('StatsOverlay: update')
+    }
+}
